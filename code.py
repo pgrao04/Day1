f6fa641ae3f9c703ff3b62bc06e994bb387af0f3
Gunvant <pgrao04@gmail.com>
2026-01-08 15:01:31 +0530

    first commit

---
+++ code.py
@@ -0,0 +1,35 @@
+## Numeric Data Types 
+a = 5
+print(type(a))
+
+b = 5.0
+print(type(b))
+
+c = 2 + 4j
+print(type(c))
+
+## Sequence Data Type 
+## 1. String Data Type 
+s = "HI, I am Gunvant Rao P"
+print(s)
+
+# check data type 
+print(type(s))
+
+# access string with index
+print(s[1])
+print(s[2])
+print(s[-1])
+
+## List data type 
+
+# Empty list
+a = []
+
+# list with int values
+a = [1, 2, 3]
+print(a)
+
+# list with mixed values int and String
+b = ["Geeks", "For", "Geeks", 4, 5]
+print(b)
